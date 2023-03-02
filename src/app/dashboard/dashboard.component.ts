c597f1217821df729460bc99ff1ebbe8ea4d6fd0
Abhinav Chaudhary <abhinav.chaudhary@happiestminds.com>
2023-03-02 09:19:32 +0530

    Added searching and pagination in table

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -22,35 +22,120 @@ export class DashboardComponent implements OnInit {
 
   private json_url: string = "assets/data.json";
   name!: String;
-  students!: studentDetails[];
+  students: studentDetails[] = [];
+  studentsOriginal: studentDetails[] = [];
   order: boolean = false;
+  searchText: any = "";
+  pageLimits: number[] = [5, 10, 15, 25];
+  pageLimit: number = 50;
+  pageDivision!: number;
+  entries: number[] = [];
+  selectedIndex: number = 0;
+  start: number = 0;
 
 
-
-  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) {
-    this.getJSON().subscribe(data => {
-      this.students = data;
-    })
-  }
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
   public getJSON(): Observable<any> {
     return this.http.get(this.json_url);
   }
 
   ngOnInit(): void {
+    this.getJSON().subscribe(data => {
+      this.students = data;
+      this.studentsOriginal = data;
+
+    })
+    this.students = this.studentsOriginal;
+    this.pageLimitChange();
     this.name = this.activatedRoute.snapshot.params["uname"];
   }
 
-  sortData() {
-    if (this.order) {
-      let newarr = this.students.sort((a, b) => a.rollno - b.rollno);
-      this.students = newarr;
+
+  sortData(formcolumn: string) {
+    if (formcolumn == 'rollno' || formcolumn == 'marks') {
+      if (this.order) {
+        let newarr = this.students.sort((a, b) => a[formcolumn] - b[formcolumn]);
+        this.students = newarr;
+      } else {
+        let newarr = this.students.sort((a, b) => b[formcolumn] - a[formcolumn]);
+        this.students = newarr;
+      }
+      this.order = !this.order;
     } else {
-      let newarr = this.students.sort((a, b) => b.rollno - a.rollno);
-      this.students = newarr;
+      if (this.order) {
+        let newarr = this.students.sort((a, b) => String(a[formcolumn as keyof studentDetails]).localeCompare(String(b[formcolumn as keyof studentDetails])));
+        this.students = newarr;
+      } else {
+        let newarr = this.students.sort((a, b) => String(b[formcolumn as keyof studentDetails]).localeCompare(String(a[formcolumn as keyof studentDetails])));
+        this.students = newarr;
+      }
+      this.order = !this.order;
     }
-    this.order = !this.order;
+
   }
+
+  searchData() {
+    if (this.searchText == "") {
+      this.ngOnInit();
+    } else {
+      this.pageLimitChange();
+      this.selectedIndex = 0;
+      this.students = this.students.filter((val) => {
+        if (val.rollno.toLocaleString().includes(this.searchText.toLowerCase()) ||
+          val.sname.toLowerCase().includes(this.searchText.toLowerCase()) ||
+          val.marks.toLocaleString().includes(this.searchText.toLowerCase()) ||
+          val.age.toLocaleString().includes(this.searchText.toLowerCase()) ||
+          val.branch.toLowerCase().includes(this.searchText.toLowerCase()))
+          return true;
+        else
+          return false;
+        // return val.sname.toLowerCase().includes(this.searchText.toLowerCase());
+      })
+    }
+  }
+
+  pageLimitChange() {
+    if (this.students.length == 0) {
+      this.pageDivision = 1;
+      this.entries = [0];
+    } else {
+      this.pageDivision = Math.ceil(this.students.length / (this.pageLimit > this.students.length ? this.students.length : this.pageLimit));
+      this.entries = Array(this.pageDivision).fill(0).map((x, i) => i);
+    }
+  }
+
+
+  changeLimit(event: Event) {
+    this.pageLimit = Number((<HTMLInputElement>event.target).value);
+    this.ngOnInit();
+  }
+
+  changePage(n: number) {
+    if (this.pageDivision * n <= this.studentsOriginal.length) {
+      this.start = this.pageLimit * n;
+    } else {
+      this.start = (this.pageLimit * n);
+      console.log(this.start)
+    }
+    this.selectedIndex = n;
+  }
+
+  previousPage() {
+    if (this.selectedIndex > 0) {
+      this.selectedIndex--;
+      this.changePage(this.selectedIndex)
+    }
+  }
+
+  nextPage() {
+    if (this.selectedIndex < this.students.length) {
+      this.selectedIndex++;
+      this.changePage(this.selectedIndex)
+
+    }
+  }
+
 
 
 }
